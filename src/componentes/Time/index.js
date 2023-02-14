5f58a082a5f421a8436defca632d7399f4da1a9f
Diogo Henrique <diogomike138@gmail.com>
2023-02-14 10:58:36 -0300

    init: Inicializando projeto.

---
+++ src/componentes/Time/index.js
@@ -0,0 +1,21 @@
+import './Time.css';
+import Colaborador from '../Colaboradores';
+
+const Time = (props)=> {
+
+    const css = { backgroundColor: props.corSecundaria };
+
+    return (
+        ( props.colaboradores.length > 0 ) ?
+        <section className='time' style={css}> 
+            <h3 style={{ borderColor: props.corPrimaria }}>{props.nome}</h3>
+            <div className='colaboradores'>
+                {props.colaboradores.map( colaborador => 
+                    <Colaborador key={colaborador.nome} corDeFundo={colaborador.corPrimaria} nome={colaborador.nome} cargo={colaborador.cargo} imagem={colaborador.imagem} />
+                )}
+            </div>
+        </section> :  ''
+    )
+}
+
+export default Time
